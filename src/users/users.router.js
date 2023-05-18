d14088b084f9c17aaaa63b1ad59b915a2fc721ee
jesuschock93 <jesuschock93@gmail.com>
2023-05-18 14:38:39 -0400

    passport-ok2

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,14 +1,17 @@
 const router = require('express').Router()
+const passport = require('passport')
+
 
 const usersServices = require('./users.services')
 
+require('../middlewares/auth.middlewares')(passport)
 
 //? Rutas Raiz >> USERS
-router.get('/', usersServices.getAllUsers)
+router.get('/', passport.authenticate('jwt', {session: false}) , usersServices.getAllUsers)
 //? Rutas dinamicas por ID
 
 router.route('/:id')
-    .get(usersServices.getUserByID)
+    .get(passport.authenticate('jwt', {session: false}),usersServices.getUserByID)
     .patch(usersServices.pathUser)
     .delete(usersServices.deleteuser)
 
